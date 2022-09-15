b20e066dfb7ba328ba8d7801093dcb8e8f4d12ce
inogutikento <inokuchi_kento@retail-ai.jp>
2022-09-15 15:48:01 +0900

    環境変数の整理

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import { useEffect, useState, Component } from 'react'
-import { ChangeEvent, KeyboardEvent, ReactElement } from 'react'
 import './App.css'
-import {supabase} from './supabaseClient'
 import { SearchForm } from './components/searchForm'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {Menu} from './components/Menu'
 import {Confirm} from './components/Confirm'
 import {LoginForm} from './components/Login'
-import { Link } from "react-router-dom";
+
 
 function App(){
-  
   return(
     <div>
       <h1>TRIAL</h1>
       <h3>生鮮スキルチェック</h3>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LoginForm/>}/>
+          <Route path="/SkillCheck/" element={<LoginForm/>}/>
           <Route path="/menu/" element={<Menu/>}/>
           <Route path='/confirm' element={<Confirm/>}/>
           <Route path='/search' element={<SearchForm/>}/>
