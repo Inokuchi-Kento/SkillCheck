cd135662001469da461eb94d3bb48495da534a77
inogutikento <inokuchi_kento@retail-ai.jp>
2022-09-15 14:03:15 +0900

    遷移機能の追加

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,7 @@ import {LoginForm} from './components/Login'
 import { Link } from "react-router-dom";
 
 function App(){
-  // function LoginForm(){
-  //   return(
-  //       <div className="loginform">
-  //           <input type="text" placeholder="ID" name="mailAddress" /><br></br>
-  //           <input type="text" placeholder="パスワード" name="password" /><br></br>
-  //           <Link to={'/menu/'}>ログイン</Link>    
-  //       </div>
-  //   )
-  // }
+  
   return(
     <div>
       <h1>TRIAL</h1>
