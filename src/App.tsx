e537f1d219d2c7cd6641e4a7f66584c1513c7e11
ishikawa-yuto <ishikawa_yuto@retail-ai.jp>
2022-09-16 10:19:13 +0900

    スキル入力画面作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,58 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Menu from "./ Menu";
+import { supabase } from './supabaseClient'
+
+type List = { name:string }
 
 function App() {
+  
+  const [list, setList] = useState<List[]>([]);
+  const [loading, setLoading] = useState(true);
+  console.log("test")
+  const get = async() =>{ 
+    try {
+      setLoading(true);
+      let {data, error} = await supabase
+      .from('employees')
+      .select('name')
+      if (error) throw error;
+      if (data) setList(data)
+    }
+    catch (error:any) {
+      alert(error.message)
+    }
+    finally {
+      setLoading(false);
+    }
+  }
+  useEffect(() => {
+    get()
+  }, []);
 
   return (
     <div className="App">
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <td>名前
+              </td>
+            </tr>
+          </thead>
+          <tbody>
+            {list.map((item) => (
+              <tr key = {item.name}>
+                <td>{item.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
       <h1>TRIAL</h1>
       <h2>生鮮スキルチェックシステム</h2>
       <div className="card">
@@ -19,6 +62,7 @@ function App() {
         <Route path="/menu" element={<Menu />} />
       </Routes>
     </BrowserRouter>
+
 
       <input type="text" placeholder="ID" name="mailAddress" /><br></br>
       <input type="text" placeholder="パスワード" name="password" /><br></br>
---
+++ src/Skill.tsx
@@ -0,0 +1,12 @@
+import './Skill.css'
+
+function Skill() {
+
+    return (
+        <div className="inputConfirmation">
+            <h1>青果　スキル入力</h1>
+            
+            <button className='submitbutton' >確認画面へ</button>
+        </div>
+    )
+}
