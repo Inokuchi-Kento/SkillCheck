af3ea5e2a2b1200026e2166b4ec8395d42978ec6
ishikawa-yuto <ishikawa_yuto@retail-ai.jp>
2022-11-03 09:30:19 +0900

    メニューの変更

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import {SearchForm} from './searchForm'
-import veg from '../assets/seika.png'
+import veg from '../assets/vegitable.png'
+import fle from '../assets/flesh.png'
+import fis from '../assets/fish.png'
+import sid from '../assets/sideDish.png'
+import men from '../assets/menu.png'
 
 
 export function Menu() {
     return (
         <div>
+            <img src={men}/>
         <h3>メニュー画面</h3>
         <Link to={"/SkillCheck/search"}>
             <img src={veg}/>
         </Link>
+            <img src={fle}/>
+            <img src={fis}/>
+            <img src={sid}/>
         <div>
             <Link to={'/SkillCheck'}>ログアウト</Link>
         </div>
