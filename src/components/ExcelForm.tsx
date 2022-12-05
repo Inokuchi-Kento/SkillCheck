a7f9f550d784f5fa45ea5c8016c487809329e2fc
ishikawa-yuto <ishikawa_yuto@retail-ai.jp>
2022-12-05 13:48:54 +0900

    エラー修正

--- src/components/ExcelForm.tsx
+++ src/components/ExcelForm.tsx
@@ -1,6 +1,6 @@
 //import download from "../icons/download.png"
-import message from "../icons/message.png"
-import upload from "../icons/upload.png"
+// import message from "../icons/message.png"
+// import upload from "../icons/upload.png"
 import "./button.css"
 
 export function ExcelForm(){
@@ -17,8 +17,8 @@ export function ExcelForm(){
             <form>
                 <p>
                 {/* <input type="image" src={download} className="excelForm" onClick={getExcel}></input> */}
-                <input type="image" src={message} className="excelForm"></input>
-                <input type="image" src={upload} className="excelForm" onClick={sendExcel}></input>
+                {/* <input type="image" src={message} className="excelForm"></input>
+                <input type="image" src={upload} className="excelForm" onClick={sendExcel}></input> */}
                 </p>
             </form>
         </div>
