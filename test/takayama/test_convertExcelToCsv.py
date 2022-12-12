6e3e6be0ea07a2d2b76dd8828410981d24f05786
hiroyuki-takayama-RAIX <takayama_hiroyuki@retail-ai.jp>
2022-12-12 09:59:53 +0900

    excelの場合の入力フォームの作成、およびCSV変換のためのpythonスクリプトの作成

--- test/takayama/test_convertExcelToCsv.py
+++ test/takayama/test_convertExcelToCsv.py
@@ -1,4 +1,7 @@
 import pandas as pd
 
-df = pd.read_excel("form.xlsx")
-df.to_csv("convertedCsv.csv")
+df = pd.read_excel("./test_form.xlsx")
+#df.drop(index=[0, 1], axis=1)
+print(df.head())
+df.to_csv("cp932.csv", encoding="cp932")
+df.to_csv("utf8.csv", encoding="utf-8")
